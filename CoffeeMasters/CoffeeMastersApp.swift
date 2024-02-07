5324e0ac8d93f2f543e8d91f49d3ce160ca93139
Monesh <moneshmaruthu@gmail.com>
2024-02-07 16:42:04 +0530

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Monesh Periyamaruthu on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
